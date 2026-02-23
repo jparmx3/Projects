ae0b4a6e70f615005de610e30ac05b41dd77ed9d
jparmx3 <jparmstrong03@gmail.com>
2026-02-23 17:39:01 -0500

    working 2/23

--- Inflation-TargetRate/TargetRate_Inflation.py
+++ Inflation-TargetRate/TargetRate_Inflation.py
@@ -2,5 +2,21 @@
 # Created by John Armstrong 
 # Goal: Looking at historical data to create consistent predictable regression model for PCE, Core PCE, CPI, Core PCI, PPI, GDP Trend
 
+from fredapi import Fred
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
+fred = Fred(api_key=os.getenv("FRED_API_KEY"))
+
+fedfunds = fred.get_series('FEDFUNDS')
+cpi = fred.get_series('CPIAUCSL')
+core_cpi = fred.get_series('CPILFESL')
+pce = fred.get_series('PCEPI')
+core_pce = fred.get_series('PCEPILFE')
+ppi = fred.get_series('PPIACO')
+gdp = fred.get_series('GDP')
+
+print(fedfunds.tail())
+print(cpi.tail())
